98969bfce69d4248fda4faf54de0639825b148dc
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-07-14 14:24:10 +0500

    Day 1 completed

---
+++ Day-1/index.js
@@ -0,0 +1,43 @@
+// Activity 1
+// Task 1
+var age = 21;
+console.log(age);
+
+// Task 2
+let name = "Abdullah";
+console.log(name);
+
+// Activity 2
+// Task 3
+const is_alive = true;
+console.log(is_alive);
+
+// Activity 3
+// Task 4
+const skills = ["HTML","CSS","JS","MERN"]
+console.log(skills)
+
+const candidate = {
+    name: name,
+    age: age,
+    is_alive: is_alive,
+    skills: skills
+}
+console.log(candidate)
+console.log(typeof age)
+console.log(typeof name);
+console.log(typeof skills);
+console.log(typeof is_alive);
+console.log(typeof candidate);
+
+// Activity 4
+// Task 5
+let userName = "John"
+console.log(userName)
+userName = "Roach"
+console.log(userName)
+
+// Activity 5
+// Task 6
+ is_alive=false
+// TypeError: Assignment to constant variable. 
