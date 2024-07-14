1d8414e38bdf61561b7223f185ab7ba7d0221b2d
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-07-14 15:13:08 +0500

    Day 2 completed

---
+++ Day-2/index.js
@@ -0,0 +1,51 @@
+// Task 1
+let n1 = 917
+let n2 = 376
+console.log(`Sum = ${n1 + n2}`)
+
+// Task 2
+console.log(`Difference = ${n1 - n2}`);
+
+// Task 3
+console.log(`Product = ${n1 * n2}`);
+
+// Task 4
+console.log(`Quotient = ${n1 / n2}`);
+
+// Task 5
+console.log(`Remainder = ${n1 % n2}`);
+
+// Task 6
+
+n1 += 83
+console.log(n1)
+
+// Task 7 
+n1 -= 100
+console.log(n1)
+
+// Task 8
+console.log(n1 < n2)
+console.log(n1 > n2)
+
+// Task 9
+let n3 = 376
+console.log(n3 >= n2)
+console.log(n3 <= n1)
+
+// Task 10
+console.log(100 == '100') // true
+console.log(100 === '100') // false
+
+// Task 11 
+console.log((n1 > n2) && false === 0) // false
+
+// Task 12
+console.log((n2 === n3) || true == 1) // true
+                         
+// Task 13                             
+console.log(!((n2 !== n3) || true == 1)); // false
+
+// Task 14 
+let n4 = -45
+n4 >= 0 ? console.log("Number is positive") : console.log("Number is negative")
